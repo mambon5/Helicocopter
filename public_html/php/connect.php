f80b373872cf196fe06f4c8d34c2026bcad6c12c
Romà <monantoniet@gmail.com>
2025-08-15 16:44:00 +0200

    Remove connect.php from repo

--- public_html/php/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "user_helico";      // id12095050_roma 
-    $password = "helico_pass";              //Joricopter_3
-    $database = "helicopter";    //id12095050_articlesdstuds
-?>
-
